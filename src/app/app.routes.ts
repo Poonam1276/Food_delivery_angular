c5e8d894bc11e17d8e5c7bea384f94257c482641
Poonam Raut <2440090@cognizant.com>
2025-10-16 18:03:41 +0530

    cart

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './component/home/home';
 
 import {SignupComponent} from './component/signup/signup'
 import { LoginComponent } from './component/login/login';
+import {CartComponent} from './component/cart/cart'
 
 
 export const routes: Routes = [
@@ -11,5 +12,6 @@ export const routes: Routes = [
   { path: 'signup', component: SignupComponent },
  
   { path: 'login', component: LoginComponent },
+  {path:'cart', component:CartComponent}
 
 ];
