689694e2e1d53ef14c3a06bcca61babba9dac8d0
Priya Jain <2439987@cognizant.com>
2025-10-18 00:21:34 +0530

    customer dashboard done

--- src/main.ts
+++ src/main.ts
@@ -10,13 +10,22 @@ import { LoginComponent } from './app/component/login/login';
 import { SignupComponent } from './app/component/signup/signup';
 import { CartComponent } from './app/component/cart/cart';
 import { CustomerDashboard } from './app/component/customer-dashboard/customer-dashboard';
+import { About } from './app/component/about/about';
+import { AdminDasboard } from './app/component/admin-dasboard/admin-dasboard';
+import { PlaceOrderComponent } from './app/component/orders/orders';
 const routes: Routes = [
  { path: '', component: HomeComponent }, // default landing page
  { path: 'menu', component: MenuComponent }, // menu page
   { path: 'signup', component: SignupComponent },
    { path: 'login', component: LoginComponent },
     {path:'cart', component:CartComponent},
-    {path: 'customer-dashboard', component: CustomerDashboard}
+    {path: 'customer-dashboard', component: CustomerDashboard}, 
+    {path:'about', component: About},
+      {path:'admin', component:AdminDasboard},
+      {
+ path: 'place-order',
+ component: PlaceOrderComponent
+}
 ];
 bootstrapApplication(App, {
  providers: [
